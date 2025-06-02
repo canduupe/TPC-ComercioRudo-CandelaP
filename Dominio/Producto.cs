4f84058d888831f7b10526004875aedf3c3dd83f
Cande Peña <cande.petra@gmail.com>
2025-06-02 00:50:25 -0300

    clase productos

---
+++ Dominio/Producto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Producto
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public decimal Precio { get; set; }
+        public int proveedor { get; set; }
+        public int marca { get; set; }
+        public int categoria { get; set; }
+        public int stockActual { get; set; }
+        public int stockMinimo { get; set; }
+        public bool activo { get; set; }
+    }
+}
